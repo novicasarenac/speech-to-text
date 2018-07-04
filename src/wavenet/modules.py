5ab36f4a2c079a6d4c77ec8c3517f2058f724750
novicasarenac <nolesarenac@yahoo.com>
2018-07-05 01:10:12 +0200

    Residual block

--- src/wavenet/modules.py
+++ src/wavenet/modules.py
@@ -67,12 +67,75 @@ class DilatedConvolution1D(torch.nn.Module):
         return output
 
 
+class ResidualBlock(torch.nn.Module):
+    """Single residual block implementation.
+
+    Attributes:
+        dilated_convolution (DilatedConvolution1D): dilated convolution layer as part of residual
+        residual_output_convolution (torch.nn.Conv1d): convolutional layer at the output of residual
+        skip_connection_convolution (torch.nn.Conv1d): convolutional layer at skip connection side of residual
+        tanh (torch.nn.Tanh): tanh layer
+        sigmoid (toch.nn.Sigmoid): sigmoid layer
+    """
+    def __init__(self, res_layers_num, skip_layers_num, dilation):
+        """Initializes residual block
+
+        Args:
+            res_layers_num (int): number of both input and output channels in residual layer
+            skip_layers_num (int): number of output layers in skip connection
+            dilation (int): value of the dilation param
+        """
+        super(ResidualBlock, self).__init__()
+
+        self.dilated_convolution = DilatedConvolution1D(res_layers_num,
+                                                        dilation)
+        self.residual_output_convolution = torch.nn.Conv1d(res_layers_num,
+                                                           res_layers_num, 1)
+        self.skip_connection_convolution = torch.nn.Conv1d(res_layers_num,
+                                                           skip_layers_num, 1)
+        self.tanh = torch.nn.Tanh()
+        self.sigmoid = torch.nn.Sigmoid()
+    
+    def forward(self, x, skip_size):
+        """Forward pass of single residual block.
+
+        Args:
+            x (tensor): input data
+            skip_size (int): size of output data at skip connection output
+        
+        Returns:
+            Output of residual block and output at skip connection side of
+            residual block.
+        """
+        dilated = self.dilated_convolution(x)
+        tanh_step = self.tanh(dilated)
+        sigmoid_step = self.sigmoid(dilated)
+        dilated_transformed = tanh_step * sigmoid_step
+
+        # Output of residual
+        residual_conv = self.residual_output_convolution(dilated_transformed)
+        input_matrix = x[:, :, -residual_conv.size(2):]
+        output = residual_conv + input_matrix
+
+        # Skip connections side output
+        skip_connection = self.skip_connection_convolution(dilated_transformed)
+        skip_connection_output = skip_connection[:, :, -skip_size:]
+
+        return output, skip_connection_output
+
+
 if __name__ == '__main__':
     inp = torch.randn(1, 20, 155)
     causal_conv = CausalConvolution1D(20, 100)
     out = causal_conv(inp)
-    print(out.size())
+    # print(out.size())
 
-    dilated_conv = DilatedConvolution1D(100, 2)
-    out2 = dilated_conv(out)
+    # dilated_conv = DilatedConvolution1D(100, 2)
+    # out2 = dilated_conv(out)
+    # print(out.size())
+
+    residual_block = ResidualBlock(100, 50, 2)
+    output, skip_out = residual_block(out, 20)
     print(out.size())
+    print(output.size())
+    print(skip_out.size())

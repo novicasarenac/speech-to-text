fcf799304eb563bcc93a17f82eb28f8f9cc94865
Predrag Njegovanovic <djaps94@gmail.com>
2018-07-07 16:10:31 +0200

    Add dedicated directory for model weights

--- src/definitions.py
+++ src/definitions.py
@@ -2,6 +2,7 @@ from os import path
 
 
 DATASET_DESTINATION = path.dirname(path.abspath(__file__ + '/../')) + '/data'
+MODEL_DESTINATION = path.dirname(path.abspath(__file__ + '/../')) + '/models'
 TRAIN = '/LibriSpeech/train-clean-100'
 TEST = '/LibriSpeech/test-clean'
 PREPROCESSED_CNN_TEST = '/preprocessed/mfcc_cnn/test/'
@@ -11,6 +12,6 @@ PREPROCESSED_RNN_TRAIN = '/preprocessed/mfcc_rnn/train/'
 LABELS_TEST = '/preprocessed/labels_test.csv'
 LABELS_TRAIN = '/preprocessed/labels_train.csv'
 
-letters = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+letters = ['<BLANK>', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-           's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '<EMP>']
+           's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']

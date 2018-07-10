d0b52b6915d25613acc0c7dae3e08461f588515f
Predrag Njegovanovic <djaps94@gmail.com>
2018-07-10 16:36:27 +0200

    Add greedy decoder

--- src/rnn_ctc/rnn_trainer.py
+++ src/rnn_ctc/rnn_trainer.py
@@ -45,7 +45,9 @@ class RNNTrainer():
         print("Starting training..")
         for epoch in range(self.num_epoches):
             loss_acc = 0
-            for sample in range(2600):
+            index_array = np.arange(len(self.data_loader))
+            np.random.shuffle(index_array)
+            for index, sample in enumerate(index_array):
                 adam.zero_grad()
                 features, labels = self.data_loader[sample]
                 labels_size = torch.IntTensor([labels.shape[0]])
@@ -62,11 +64,15 @@ class RNNTrainer():
                 adam.step()
                 # accumulate loss
                 loss_acc += loss.data[0]
-                if (sample + 1) % 100 == 0:
-                    print("Training example {} -- Loss: {:.4f}".format(sample + 1, loss_acc/(sample + 1)))
+                if (index + 1) % 100 == 0:
+                    print("Training example {} -- Loss: {:.4f}".format(index + 1, loss_acc/(index + 1)))
+
+                if(index % 2000) == 0:
+                    torch.save(model.state_dict(), MODEL_DESTINATION + RNN_WEIGHTS)
+
 
             print("Epoch: {} ---> Mean loss: {:.4f}".format(epoch + 1, loss_acc/self.data_loader.__len__()))
-        torch.save(model.state_dict(), MODEL_DESTINATION + RNN_WEIGHTS)
+            torch.save(model.state_dict(), MODEL_DESTINATION + RNN_WEIGHTS)
         print("Training finished..")
         return model
 
@@ -76,10 +82,19 @@ class RNNTrainer():
     def inference(self, model, output):
         pass
 
+    def greedy_decoding(self, probs, letter2ind):
+        indices = []
+        letter2ind = make_encodings()
+        for i in range(probs.shape[0]):
+            indices.append(np.argmax(probs[i, :]))
+
+        print("True: {}".format(decode(label, letter2ind)))
+        print("Predicted: {}".format(decode(indices, letter2ind)))
+
 
 if __name__ == "__main__":
     args = {
-        "num_epoches": 1,
+        "num_epoches": 10,
         "labels_path": DATASET_DESTINATION + LABELS_TRAIN,
         "features_path": DATASET_DESTINATION + PREPROCESSED_RNN_TRAIN,
         "vocab_size": 28,
@@ -94,22 +109,13 @@ if __name__ == "__main__":
                       trainer.num_layers,
                       trainer.hidden_units,
                       trainer.vocabulary_size).to(trainer.device)
+    softmax = torch.nn.Softmax(dim=2)
     model.load_state_dict(torch.load(MODEL_DESTINATION + RNN_WEIGHTS))
-    mfcc, label = trainer.data_loader.__getitem__(7)
+    model.eval()
+    mfcc, label = trainer.data_loader[0]
     probs = model(mfcc, trainer.device)
-    probs = probs.squeeze(1)
-    decoder = ctcdecode.CTCBeamDecoder(letters,
-                                       num_processes=multiprocessing.cpu_count(),
-                                       blank_id=0)
+    probs = softmax(probs)
+    probs = probs.squeeze(1).detach().cpu().numpy()
 
-    # output, score, timesteps, out_seq_len = decoder.decode(probs)
-    probs = torch.squeeze(probs).detach().cpu().numpy()
-    indices = []
-    letter2ind = make_encodings()
-    for i in range(probs.shape[0]):
-        indices.append(np.argmax(probs[i, :]))
-
-    print("True: {}".format(decode(label, letter2ind)))
-    print("Predicted: {}".format(decode(indices, letter2ind)))
-
-
+    # greedy decoding
+    trainer.greedy_decoding(probs, letter2ind)
